69d86a9e5fba002d211d83144c06fb191b1185d9
Zhu Shaocong <2030104470@qq.com>
2021-06-22 13:51:38 +0800

    Initial commit

---
+++ src/test/java/com/example/demo04/Demo04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
